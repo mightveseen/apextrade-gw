e095d3d73035030a77742806fd2567cfe3fb6825
mightveseen <mightveseen@gmail.com>
2026-01-29 16:45:32 +0100

    Init

---
+++ src/test/java/com/apextrade/gw/TemplateTest.java
@@ -0,0 +1,12 @@
+package com.apextrade.gw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
